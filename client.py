ba8b683c5c56b9bd04bdb88acb3042be81ef90a2
CruizK <cruiz.kusman@gmail.com>
2021-03-24 11:35:35 -0500

    Talking is a-go

--- client.py
+++ client.py
@@ -11,29 +11,60 @@ PORT = int(sys.argv[2])
 
 isTalking = False
 
+SERVER_RESP = 0
+CHAT_MSG = 1
+TALK_REQ = 2
+ACCEPT_REQ = 3
+TALK_ESTAB = 4
+CHAT_DESTROY = 5
+CLOSE_CONN = 6
+
+BUFF = 1024
+
+def decode_pkt(pkt):
+  pkt = bytearray(pkt)
+  msg_type = int.from_bytes(pkt[0:1], byteorder="big")
+  msg = pkt[1:].decode('utf-8')
+  return (msg_type, msg)
+
 def recieve(s):
+  global isTalking
   while True:
-    data = s.recv(1024).decode('utf-8')
-    if 'talk request' in data.lower():
-      print('\n{0}'.format(data))
-    else:
-      print(data)
+    data = s.recv(BUFF)
+    pkt = decode_pkt(data)
+    if pkt[0] == SERVER_RESP:
+      print("Server: {0}".format(pkt[1]), end='\n> ')
+    elif pkt[0] == TALK_ESTAB:
+      isTalking = True
+      print(pkt[1])
+    elif pkt[0] == TALK_REQ:
+      print(pkt[1], end='\n> ')
+    elif pkt[0] == ACCEPT_REQ:
+      isTalking = True
+      print('')
+    elif pkt[0] == CHAT_MSG:
+      print(pkt[1])
+    elif pkt[0] == CHAT_DESTROY:
+      isTalking = False
+      print(pkt[1], end='\n> ')
+    elif pkt[0] == CLOSE_CONN:
+      break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         
   s.connect((HOST, PORT))
-  print("> Server: Enter your name")
+  isConnected = True
+  print(s.recv(BUFF).decode('utf-8'))
   name = input('> ')
   s.sendall(bytes(name, 'utf-8'))
-
   thread = threading.Thread(target=recieve, args=(s,))
   thread.start()
   user_in = ''
-  while user_in != "exit":
+  while user_in != "exit" or isTalking == True:
     if isTalking == True:
       user_in = input('')
     else:
       user_in = input('> ').lower()
-    if 'talk' in user_in or 'accept' in user_in:
-      isTalking = True
     s.sendall(bytes(user_in, 'utf-8'))
+  thread.join()
+  
--- server.py
+++ server.py
@@ -11,39 +11,122 @@ if len(sys.argv) != 2:
 HOST = '0.0.0.0'
 PORT = int(sys.argv[1])
 
+
+SERVER_RESP = 0
+CHAT_MSG = 1
+TALK_REQ = 2
+ACCEPT_REQ = 3
+TALK_ESTAB = 4
+CHAT_DESTROY = 5
+CLOSE_CONN = 6
+
+BUFF = 1024
+
 clients = {}
 chats = {}
 
 class Chat:
-  def __init__(self):
+  def __init__(self, host):
+    self.host = host
     self.clients = []
-    self.rung = []
 
 class Client:
   def __init__(self, name, conn, ip):
     self.name = name
     self.talking = False
+    self.ringing = []
     self.conn = conn
     self.ip = ip
 
 def chat_thread(my_client, chat):
-  print("Starting thread for client {0}".format(my_client.name))
   msg = ''
-  while msg.lower() != 'exit':
-    msg = my_client.conn.recv(1024).decode('utf-8')
+  while msg.lower() != 'exit' and my_client.talking:
+    msg = my_client.conn.recv(BUFF).decode('utf-8')
+    if chat.host not in chats:
+      return
+    print('{0}: {1}', my_client.name, msg)
     for client in chat.clients:
       if client != my_client:
-        client.conn.sendall('{0}: {1}'.format(client.name, msg).encode('utf-8'))
+        client.conn.sendall(encode_pkt(CHAT_MSG, '{0}: {1}'.format(client.name, msg)))
+  
+  if chat.host in chats:
+    for client in chat.clients:
+      client.conn.sendall(encode_pkt(CHAT_DESTROY, 'Connection Terminated'))
+      client.talking = False
+    del chats[chat.host]
 
+
+def encode_pkt(msg_type, msg):
+  pkt = bytearray()
+  pkt.append(msg_type)
+  pkt.extend(msg.encode('utf-8'))
+  return pkt
+
+def spawn_chat_thread(client, chat):
+  thread = threading.Thread(target=chat_thread, args=(client,chat,))
+  thread.start()
 
 
 def on_client_conn(conn):
   print('Accepted Request from ip: {0}'.format(addr[0]))
-  #conn.sendall(b'>Server: Enter your name')
+  conn.sendall(b'>Server: Enter your name')
   print('Message Sent Asking For Client-Name')
-  name = conn.recv(1024).decode('utf-8').lower()
+  name = conn.recv(BUFF).decode('utf-8').lower()
   clients[name] = Client(name, conn, addr[0])
   print('{0} named {1}'.format(addr[0], name))
+  
+
+  while True:
+    if clients[name].talking == True:
+      pass
+    else: 
+      rec_line = conn.recv(BUFF).decode('utf-8')
+      rec_line = rec_line.replace(',', '').split(' ')
+
+      command = rec_line[0].lower()
+
+      if command == 'exit':
+        conn.sendall(encode_pkt(CLOSE_CONN, ''))
+        conn.close()
+        break
+      elif command == 'talk':
+        if len(rec_line) < 2:
+          conn.sendall(encode_pkt(SERVER_RESP, "talk <client_name>"))
+          continue
+        reciver = rec_line[1]
+        print(reciver)
+        if reciver not in clients:
+          conn.sendall(encode_pkt(SERVER_RESP, "Invalid Client"))
+        else:
+          clients[reciver].conn.sendall(encode_pkt(TALK_REQ, 'Talk Request from {0}@{1}. Respond with "accept {0}@{1}"'.format(name, addr[0])))
+          conn.sendall(encode_pkt(SERVER_RESP, "Ringing {0}".format(reciver)))
+          clients[name].ringing.append(reciver)
+          chats[name] = Chat(name)
+          chats[name].clients.append(clients[name])
+          clients[name].talking = True
+          spawn_chat_thread(clients[name], chats[name])
+      elif command == 'accept':
+        if len(rec_line) < 2:
+          conn.sendall(encode_pkt(SERVER_RESP, "accept <client_name>@<client_ip>"))
+          continue
+        caller_name = rec_line[1].split('@')[0]
+        caller = clients[caller_name]
+        if caller_name not in clients or name not in caller.ringing:
+          conn.sendall(encode_pkt(SERVER_RESP, "Invalid Caller"))
+        else:
+          caller.ringing.remove(name)
+          chat = chats[caller_name]
+          chat.clients.append(clients[name])
+          caller.conn.sendall(encode_pkt(TALK_ESTAB, "Talk connection established with {0}".format(name)))
+          conn.sendall(encode_pkt(ACCEPT_REQ, ''))
+          clients[name].talking = True
+          spawn_chat_thread(clients[name], chat)
+      else:
+        conn.sendall(encode_pkt(SERVER_RESP, "Invalid Command"))
+
+
+
+"""
   while clients[name].talking == False:
     statement = conn.recv(1024).decode('utf-8')
     statement = statement.replace(',', '').split(' ')
@@ -79,6 +162,7 @@ def on_client_conn(conn):
     else:
       conn.sendall(b'>Server: Invalid Command')
       continue
+    """
 
       
         
